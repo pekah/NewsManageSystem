c585690dc3adbc7233cf87299680a498da56e353
Yilinz <Yilinz@shenzhen.lifeix.com>
2015-04-29 18:05:09 +0800

    将新闻url提出出来

--- src/main/java/com/zyl/controller/SpiderController.java
+++ src/main/java/com/zyl/controller/SpiderController.java
@@ -24,18 +24,24 @@ public class SpiderController {
 	@Qualifier("usersService")
 	private UsersService userSerivce;
 	
+	private static String TAIJIQUAN = "http://www.soku.com/search_video/q_%E5%A4%AA%E6%9E%81%E6%8B%B3_orderby_2";
+	private static String SQUAREDANCE = "http://www.soku.com/search_video/q_%E5%B9%BF%E5%9C%BA%E8%88%9E_orderby_2";
+	private static String RUMOUR = "http://www.liuyanbaike.com/category/";
+	private static String YCWB = "http://news.ycwb.com/n_bd_gz";
+	private static String NATION = "http://www.chinanews.com/world.shtml";
+	private static String HISTORY = "http://view.news.qq.com/l/history_new/history_article/list201206115621.htm";
+	
+	
 	//抓取太极拳视频
 	@RequestMapping("news-taijiquan")
 	@ResponseBody
 	public ModelAndView addTaijiquan(){
 		ModelAndView mv = new ModelAndView();
-		String taijiquanUrl = "http://www.soku.com/search_video/q_%E5%A4%AA%E6%9E%81%E6%8B%B3_orderby_2";
-		
 		int total = 0;
 		
 		for(int i = 1; i <= FATCHPAGE; i++){
-			taijiquanUrl = taijiquanUrl + "_page_" + i;
-			total += spiderSerive.addYoukuVideo(taijiquanUrl,"太极拳");
+			TAIJIQUAN = TAIJIQUAN + "_page_" + i;
+			total += spiderSerive.addYoukuVideo(TAIJIQUAN,"太极拳");
 		}
 		
 		List<News> newsList = null;
@@ -56,13 +62,12 @@ public class SpiderController {
 	@RequestMapping("news-squareDance")
 	public ModelAndView addSquareDance(){
 		ModelAndView mv = new ModelAndView();
-		String squareDanceUrl = "http://www.soku.com/search_video/q_%E5%B9%BF%E5%9C%BA%E8%88%9E_orderby_2";
 		
 		int total = 0;
 		
 		for(int i = 1; i <= FATCHPAGE; i++){
-			squareDanceUrl = squareDanceUrl + "_page_" + i;
-			total += spiderSerive.addYoukuVideo(squareDanceUrl,"广场舞");
+			SQUAREDANCE = SQUAREDANCE + "_page_" + i;
+			total += spiderSerive.addYoukuVideo(SQUAREDANCE,"广场舞");
 		}
 		
 		List<News> newsList = null;
@@ -83,13 +88,10 @@ public class SpiderController {
 	@RequestMapping("news-rumour")
 	public ModelAndView addRumour(){
 		ModelAndView mv = new ModelAndView();
-		String url = "http://www.liuyanbaike.com/category/";
-		
 		int total = 0;
-		
 		for(int i = 1; i <= FATCHPAGE; i++){
-			url = url + "?page=" + i;
-			total += spiderSerive.addRumour(url,"流言百科");
+			RUMOUR = RUMOUR + "?page=" + i;
+			total += spiderSerive.addRumour(RUMOUR,"流言百科");
 		}
 		
 		List<News> newsList = null;
@@ -110,7 +112,6 @@ public class SpiderController {
 	@RequestMapping("news-ycwb")
 	public ModelAndView addYCWB(){
 		ModelAndView mv = new ModelAndView();
-		String url = "http://news.ycwb.com/n_bd_gz";
 		
 		int total = 0;
 		
@@ -118,12 +119,12 @@ public class SpiderController {
 			System.out.println("第" + i + "页");
 			
 			if(i == 1){
-				url = url + ".htm";
+				YCWB = YCWB + ".htm";
 			}else{
-				url = url + "_" + i + ".htm";
+				YCWB = YCWB + "_" + i + ".htm";
 			}
 			
-			total += spiderSerive.addYCWB(url, "羊城晚报");
+			total += spiderSerive.addYCWB(YCWB, "羊城晚报");
 		}
 		
 		List<News> newsList = null;
@@ -145,11 +146,10 @@ public class SpiderController {
 	@RequestMapping("news-nation")
 	public ModelAndView addNation(){
 		ModelAndView mv = new ModelAndView();
-		String url = "http://www.chinanews.com/world.shtml";
 		
 		int total = 0;
 		
-		total += spiderSerive.addNation(url, "国际热点");
+		total += spiderSerive.addNation(NATION, "国际热点");
 		
 		List<News> newsList = null;
 		if(total > 0){
@@ -167,13 +167,11 @@ public class SpiderController {
 	
 	//抓取历史新闻
 	@RequestMapping("news-history")
-	public ModelAndView addZhihuRecommend(){
+	public ModelAndView addHistory(){
 		ModelAndView mv = new ModelAndView();
-		String url = "http://view.news.qq.com/l/history_new/history_article/list201206115621.htm";
-		
 		int total = 0;
 		
-		total += spiderSerive.addHistory(url, "历史新闻");
+		total += spiderSerive.addHistory(HISTORY, "历史新闻");
 		
 		List<News> newsList = null;
 		if(total > 0){
@@ -191,4 +189,30 @@ public class SpiderController {
 	
 	
 	//一键抓取，多线程
+	@RequestMapping("news-all")
+	public ModelAndView addAllNews(){
+		ModelAndView mv = new ModelAndView();
+		int total = 0;
+		
+		spiderSerive.addYoukuVideo(TAIJIQUAN,"太极拳");
+		spiderSerive.addYoukuVideo(SQUAREDANCE,"广场舞");
+		spiderSerive.addRumour(RUMOUR,"流言百科");
+		spiderSerive.addYCWB(YCWB,"羊城晚报");
+		spiderSerive.addNation(NATION, "国际热点");
+		spiderSerive.addHistory(HISTORY, "历史新闻");
+		
+		List<News> newsList = null;
+		if(total > 0){
+			newsList = userSerivce.getLatestNewsByCateName("历史新闻", total);
+		}
+		
+		mv.addObject("cname","历史新闻");
+		mv.addObject("total",total);
+		mv.addObject("newsList", newsList);
+		
+		mv.setViewName("admin/spider-index.jsp");
+		
+		return mv;	
+	}		
+	
 }
